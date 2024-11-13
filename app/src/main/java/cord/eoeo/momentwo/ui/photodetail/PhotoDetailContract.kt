424d71a87c84e0a9157c6dd76b1a35d8fd81e0b5
eoeo0326 <eoeo0326@gmail.com>
2024-11-13 21:28:00 +0900

    issue #92 feat: PhotoDetailContract State, Event 추가 / PhotoDetailViewModel Repository, UseCase 추가, 기능 연결

--- app/src/main/java/cord/eoeo/momentwo/ui/photodetail/PhotoDetailContract.kt
+++ app/src/main/java/cord/eoeo/momentwo/ui/photodetail/PhotoDetailContract.kt
@@ -1,8 +1,13 @@
 package cord.eoeo.momentwo.ui.photodetail
 
+import androidx.paging.PagingData
 import cord.eoeo.momentwo.ui.UiEffect
 import cord.eoeo.momentwo.ui.UiEvent
 import cord.eoeo.momentwo.ui.UiState
+import cord.eoeo.momentwo.ui.model.CommentItem
+import cord.eoeo.momentwo.ui.model.DescriptionItem
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 
 class PhotoDetailContract {
     data class State(
@@ -10,7 +15,14 @@ class PhotoDetailContract {
         val subAlbumId: Int = -1,
         val photoId: Int = -1,
         val photoUrl: String = "",
+        val descriptionItem: DescriptionItem = DescriptionItem("", "", "", ""),
+        val likeCount: Int = 0,
+        val likeLastUpdated: Long = 0L,
+        val commentPagingData: Flow<PagingData<CommentItem>> = emptyFlow(),
+        val isWriteDescMode: Boolean = false,
         val isMenuVisible: Boolean = true,
+        val isDescriptionVisible: Boolean = false,
+        val isCommentVisible: Boolean = false,
         val isLiked: Boolean = false,
         val isLoading: Boolean = false,
         val isSuccess: Boolean = false,
@@ -20,11 +32,28 @@ class PhotoDetailContract {
     sealed interface Event : UiEvent {
         data object OnToggleIsMenuVisible : Event
         data object OnToggleIsLiked : Event
+        data object OnGetLikeCount : Event
+        data class OnChangeIsDescriptionVisible(val isDescriptionVisible: Boolean) : Event
+        data class OnChangeIsWriteDescMode(val isWriteDescMode: Boolean) : Event
+        data object OnGetDescription : Event
+        data class OnWriteDescription(val title: String, val contents: String) : Event
+        data class OnEditDescription(val title: String, val contents: String) : Event
+        data object OnDeleteDescription : Event
+        data class OnChangeIsCommentVisible(val isCommentVisible: Boolean) : Event
+        data object OnScrollCommentToTop : Event
+        data class OnWriteComment(val comment: String) : Event
+        data class OnEditComment(val commentId: Int, val comment: String) : Event
+        data class OnDeleteComment(val commentId: Int) : Event
+        data object OnDownloadPhoto : Event
         data object OnBack : Event
-        data class OnError(val errorMessage: String) : Event
+        data class OnShowSnackbar(val message: String) : Event
     }
 
     sealed interface Effect : UiEffect {
+        data object RefreshCommentPagingData : Effect
+        data object ScrollCommentToBottom : Effect
+        data object ScrollCommentToTop : Effect
+        data class ToggleSystemBars(val isVisible: Boolean) : Effect
         data object PopBackStack : Effect
         data class ShowSnackbar(val message: String) : Effect
     }
--- app/src/main/java/cord/eoeo/momentwo/ui/photodetail/PhotoDetailViewModel.kt
+++ app/src/main/java/cord/eoeo/momentwo/ui/photodetail/PhotoDetailViewModel.kt
@@ -1,27 +1,42 @@
 package cord.eoeo.momentwo.ui.photodetail
 
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
+import cord.eoeo.momentwo.data.comment.CommentRepository
+import cord.eoeo.momentwo.data.description.DescriptionRepository
+import cord.eoeo.momentwo.data.like.LikeRepository
+import cord.eoeo.momentwo.domain.photo.DownloadPhotoUseCase
 import cord.eoeo.momentwo.ui.BaseViewModel
 import cord.eoeo.momentwo.ui.MomentwoDestination
+import cord.eoeo.momentwo.ui.model.DescriptionItem
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PhotoDetailViewModel @Inject constructor(
+    private val descriptionRepository: DescriptionRepository,
+    private val likeRepository: LikeRepository,
+    private val commentRepository: CommentRepository,
+    private val downloadPhotoUseCase: DownloadPhotoUseCase,
     savedStateHandle: SavedStateHandle,
 ) : BaseViewModel<PhotoDetailContract.State, PhotoDetailContract.Event, PhotoDetailContract.Effect>() {
     init {
         val (albumId, subAlbumId, photoId, photoUrl) = savedStateHandle.toRoute<MomentwoDestination.PhotoDetail>()
 
-        setState(
-            uiState.value.copy(
-                albumId = albumId,
-                subAlbumId = subAlbumId,
-                photoId = photoId,
-                photoUrl = photoUrl,
+        viewModelScope.launch {
+            setState(
+                uiState.value.copy(
+                    albumId = albumId,
+                    subAlbumId = subAlbumId,
+                    photoId = photoId,
+                    photoUrl = photoUrl,
+                    commentPagingData = commentRepository.getCommentPagingData(albumId, photoId, 10),
+                )
             )
-        )
+        }
     }
 
     override fun createInitialState(): PhotoDetailContract.State = PhotoDetailContract.State()
@@ -29,20 +44,195 @@ class PhotoDetailViewModel @Inject constructor(
     override fun handleEvent(newEvent: PhotoDetailContract.Event) {
         when (newEvent) {
             is PhotoDetailContract.Event.OnToggleIsMenuVisible -> {
-                with (uiState.value) { setState(copy(isMenuVisible = isMenuVisible.not())) }
+                with(uiState.value) {
+                    setState(copy(isMenuVisible = isMenuVisible.not()))
+                    setEffect { PhotoDetailContract.Effect.ToggleSystemBars(isMenuVisible.not()) }
+                }
             }
 
             is PhotoDetailContract.Event.OnToggleIsLiked -> {
-                with (uiState.value) { setState(copy(isLiked = isLiked.not())) }
-                // TODO: 1초 딜레이 후 좋아요 상태 변경 요청 추가
+                viewModelScope.launch {
+                    with(uiState.value) {
+                        val currentTime = System.currentTimeMillis()
+
+                        if (currentTime - likeLastUpdated > 1000L) {
+                            if (isLiked.not()) {
+                                setState(copy(isLiked = true, likeLastUpdated = currentTime, likeCount = likeCount + 1))
+                                likeRepository.requestDoLike(albumId, photoId)
+                            } else {
+                                setState(
+                                    copy(
+                                        isLiked = false,
+                                        likeLastUpdated = currentTime,
+                                        likeCount = likeCount - 1
+                                    )
+                                )
+                                likeRepository.requestUndoLike(albumId, photoId)
+                            }.onFailure { exception ->
+                                // TODO
+                                Log.e("Desc", "OnToggleIsLiked Failure", exception)
+                            }
+                        }
+                    }
+                }
+            }
+
+            is PhotoDetailContract.Event.OnGetLikeCount -> {
+                viewModelScope.launch {
+                    with(uiState.value) {
+                        likeRepository.getLikeCount(albumId, photoId)
+                            .onSuccess { count ->
+                                setState(copy(likeCount = count))
+                            }.onFailure { exception ->
+                                // TODO
+                                Log.e("Desc", "getLikeCount Failure", exception)
+                            }
+                    }
+                }
+            }
+
+            is PhotoDetailContract.Event.OnChangeIsDescriptionVisible -> {
+                with(uiState.value) { setState(copy(isDescriptionVisible = newEvent.isDescriptionVisible)) }
+            }
+
+            is PhotoDetailContract.Event.OnChangeIsWriteDescMode -> {
+                with(uiState.value) { setState(copy(isWriteDescMode = newEvent.isWriteDescMode)) }
+            }
+
+            is PhotoDetailContract.Event.OnGetDescription -> {
+                viewModelScope.launch {
+                    with(uiState.value) {
+                        descriptionRepository.getDescription(albumId, photoId)
+                            .onSuccess { desc ->
+                                setState(copy(descriptionItem = desc))
+                            }.onFailure { exception ->
+                                // TODO
+                                Log.e("Desc", "getDescription Failure", exception)
+                            }
+                    }
+                }
+            }
+
+            is PhotoDetailContract.Event.OnWriteDescription -> {
+                viewModelScope.launch {
+                    with(uiState.value) {
+                        val descString = StringBuilder().appendLine(newEvent.title).append(newEvent.contents).toString()
+
+                        descriptionRepository.writeDescription(albumId, photoId, descString)
+                            .onSuccess {
+                                setEvent(PhotoDetailContract.Event.OnGetDescription)
+                                setEvent(PhotoDetailContract.Event.OnChangeIsWriteDescMode(false))
+                            }.onFailure { exception ->
+                                // TODO
+                                Log.e("Desc", "writeDescription Failure", exception)
+                            }
+                    }
+                }
+            }
+
+            is PhotoDetailContract.Event.OnEditDescription -> {
+                viewModelScope.launch {
+                    with(uiState.value) {
+                        val descString = StringBuilder().appendLine(newEvent.title).append(newEvent.contents).toString()
+
+                        descriptionRepository.editDescription(albumId, photoId, descString)
+                            .onSuccess {
+                                setEvent(PhotoDetailContract.Event.OnGetDescription)
+                            }.onFailure { exception ->
+                                // TODO
+                                Log.e("Desc", "editDescription Failure", exception)
+                            }
+                    }
+                }
+            }
+
+            is PhotoDetailContract.Event.OnDeleteDescription -> {
+                viewModelScope.launch {
+                    with(uiState.value) {
+                        descriptionRepository.deleteDescription(albumId, photoId)
+                            .onSuccess {
+                                setState(copy(descriptionItem = DescriptionItem("", "", "", "")))
+                            }.onFailure { exception ->
+                                // TODO
+                                Log.e("Desc", "deleteDescription Failure", exception)
+                            }
+                    }
+                }
+            }
+
+            is PhotoDetailContract.Event.OnChangeIsCommentVisible -> {
+                with(uiState.value) { setState(copy(isCommentVisible = newEvent.isCommentVisible)) }
+            }
+
+            is PhotoDetailContract.Event.OnScrollCommentToTop -> {
+                setEffect { PhotoDetailContract.Effect.ScrollCommentToTop }
+            }
+
+            is PhotoDetailContract.Event.OnWriteComment -> {
+                viewModelScope.launch {
+                    with(uiState.value) {
+                        commentRepository.writeComment(albumId, photoId, newEvent.comment)
+                            .onSuccess {
+                                setEffect { PhotoDetailContract.Effect.RefreshCommentPagingData }
+                                setEffect { PhotoDetailContract.Effect.ScrollCommentToBottom }
+                            }.onFailure { exception ->
+                                // TODO
+                                Log.e("Comment", "writeComment Failure", exception)
+                            }
+                    }
+                }
+            }
+
+            is PhotoDetailContract.Event.OnEditComment -> {
+                viewModelScope.launch {
+                    with(uiState.value) {
+                        commentRepository.editComment(albumId, newEvent.commentId, newEvent.comment)
+                            .onSuccess {
+                                setEffect { PhotoDetailContract.Effect.RefreshCommentPagingData }
+                            }.onFailure { exception ->
+                                // TODO
+                                Log.e("Comment", "editComment Failure", exception)
+                            }
+                    }
+                }
+            }
+
+            is PhotoDetailContract.Event.OnDeleteComment -> {
+                viewModelScope.launch {
+                    with(uiState.value) {
+                        commentRepository.deleteComment(albumId, newEvent.commentId)
+                            .onSuccess {
+                                setEffect { PhotoDetailContract.Effect.RefreshCommentPagingData }
+                            }.onFailure { exception ->
+                                // TODO
+                                Log.e("Comment", "writeComment Failure", exception)
+                            }
+                    }
+                }
+            }
+
+            is PhotoDetailContract.Event.OnDownloadPhoto -> {
+                viewModelScope.launch {
+                    downloadPhotoUseCase(uiState.value.photoUrl)
+                        .onSuccess {
+                            // TODO
+                            setEvent(PhotoDetailContract.Event.OnShowSnackbar("사진 다운로드를 완료했습니다"))
+                            Log.d("Photo", "downloadPhoto Success")
+                        }
+                        .onFailure { exception ->
+                            // TODO
+                            setEvent(PhotoDetailContract.Event.OnShowSnackbar("사진 다운로드에 실패했습니다"))
+                            Log.e("Photo", "downloadPhoto Failure", exception)
+                        }
+                }
             }
 
             is PhotoDetailContract.Event.OnBack -> {
                 setEffect { PhotoDetailContract.Effect.PopBackStack }
             }
 
-            is PhotoDetailContract.Event.OnError -> {
-                setEffect { PhotoDetailContract.Effect.ShowSnackbar(newEvent.errorMessage) }
+            is PhotoDetailContract.Event.OnShowSnackbar -> {
+                setEffect { PhotoDetailContract.Effect.ShowSnackbar(newEvent.message) }
             }
         }
     }
