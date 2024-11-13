36f24403209500fe41254e4b287d6cafd848a6c4
eoeo0326 <eoeo0326@gmail.com>
2024-11-13 20:58:00 +0900

    issue #87 feat: CommentItem 추가 (UI Model)

---
+++ app/src/main/java/cord/eoeo/momentwo/ui/model/CommentItem.kt
@@ -0,0 +1,9 @@
+package cord.eoeo.momentwo.ui.model
+
+data class CommentItem(
+    val id: Int,
+    val nickname: String,
+    val userProfileImage: String,
+    val comment: String,
+    val date: String,
+)
