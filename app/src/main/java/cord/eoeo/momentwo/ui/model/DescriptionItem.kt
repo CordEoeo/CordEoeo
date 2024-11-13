737f7c99acd1852ad2e9110a488c880455189706
eoeo0326 <eoeo0326@gmail.com>
2024-11-13 20:57:46 +0900

    issue #87 feat: DescriptionItem 추가 (UI Model)

---
+++ app/src/main/java/cord/eoeo/momentwo/ui/model/DescriptionItem.kt
@@ -0,0 +1,8 @@
+package cord.eoeo.momentwo.ui.model
+
+data class DescriptionItem(
+    val nickname: String,
+    val userProfileImage: String,
+    val description: String,
+    val date: String,
+)
