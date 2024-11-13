973d5c1acc8e149fb7dd7394f1301b351e45955a
eoeo0326 <eoeo0326@gmail.com>
2024-11-13 20:23:30 +0900

    issue #87 feat: CommentService 구현 (+ Request, Response Data Type)

---
+++ app/src/main/java/cord/eoeo/momentwo/data/model/Comment.kt
@@ -0,0 +1,48 @@
+package cord.eoeo.momentwo.data.model
+
+import com.squareup.moshi.JsonClass
+import cord.eoeo.momentwo.ui.model.CommentItem
+
+@JsonClass(generateAdapter = true)
+data class CreateComment(
+    val albumId: Int,
+    val photoId: Int,
+    val comments: String,
+)
+
+@JsonClass(generateAdapter = true)
+data class EditComment(
+    val albumId: Int,
+    val commentId: Int,
+    val editComments: String,
+)
+
+@JsonClass(generateAdapter = true)
+data class DeleteComment(
+    val albumId: Int,
+    val commentId: Int,
+)
+
+@JsonClass(generateAdapter = true)
+data class CommentPage(
+    val commentsList: List<Comment>,
+    val nextCursor: Int?,
+)
+
+@JsonClass(generateAdapter = true)
+data class Comment(
+    val id: Int,
+    val nickname: String,
+    val userProfileImage: String,
+    val comment: String,
+    val date: String,
+) {
+    fun mapToCommentItem(): CommentItem =
+        CommentItem(
+            id = id,
+            nickname = nickname,
+            userProfileImage = userProfileImage,
+            comment = comment,
+            date = date,
+        )
+}
