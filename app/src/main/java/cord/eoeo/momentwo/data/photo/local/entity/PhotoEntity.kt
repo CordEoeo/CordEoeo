ba24f0058bc5904099b08df57f075cdf36361188
eoeo0326 <eoeo0326@gmail.com>
2024-11-14 14:20:15 +0900

    issue #96 feat: PhotoEntity 수정, PhotoDao 추가, PhotoDataSource 기능 구현

--- app/src/main/java/cord/eoeo/momentwo/data/photo/local/entity/PhotoEntity.kt
+++ app/src/main/java/cord/eoeo/momentwo/data/photo/local/entity/PhotoEntity.kt
@@ -3,18 +3,20 @@ package cord.eoeo.momentwo.data.photo.local.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import cord.eoeo.momentwo.ui.model.ImageItem
+import cord.eoeo.momentwo.ui.model.PhotoItem
 
 @Entity(tableName = "photo")
 data class PhotoEntity(
     @PrimaryKey val id: Int,
     @ColumnInfo(name = "album_id") val albumId: Int,
     @ColumnInfo(name = "sub_album_id") val subAlbumId: Int,
-    @ColumnInfo(name = "image_url") val imageUrl: String,
+    @ColumnInfo(name = "photo_url") val photoUrl: String,
+    @ColumnInfo(name = "is_liked") val isLiked: Boolean,
 ) {
-    fun mapToImageItem(): ImageItem =
-        ImageItem(
+    fun mapToPhotoItem(): PhotoItem =
+        PhotoItem(
             id = id,
-            imageUrl = imageUrl,
+            photoUrl = photoUrl,
+            isLiked = isLiked,
         )
 }
