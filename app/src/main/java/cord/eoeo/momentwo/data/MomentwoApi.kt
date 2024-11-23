52276dd989287afb90b0071ec0d332b9dca3f2b6
eoeo0326 <eoeo0326@gmail.com>
2024-11-23 21:37:29 +0900

    issue #98, #32 feat: MomentwoApi URL 추가, 수정
    친구 목록 페이징 URL Param 설명 추가
    유저 검색 URL 추가

--- app/src/main/java/cord/eoeo/momentwo/data/MomentwoApi.kt
+++ app/src/main/java/cord/eoeo/momentwo/data/MomentwoApi.kt
@@ -75,7 +75,10 @@ object MomentwoApi {
     const val POST_FRIEND_REQUEST = "/friendship/request"
     const val DELETE_FRIEND_REQUEST = "/friendship/request/cancel"
     const val POST_RESPONSE_FRIEND_REQUEST = "/friendship/response"
-    const val GET_FRIEND_LIST = "/friendship"
+    /** Require @Query("size"), @Query("cursor") */
+    const val GET_FRIEND_PAGE = "/friendship"
     const val GET_SENT_FRIEND_REQUESTS = "/friendship/send"
     const val GET_RECEIVED_FRIEND_REQUESTS = "/friendship/receive"
+    /** Require @Query("nickname"), @Query("page"), @Query("size") */
+    const val GET_SEARCH_USERS = "/users/search"
 }
