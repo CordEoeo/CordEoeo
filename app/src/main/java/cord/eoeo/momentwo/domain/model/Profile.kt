ef94d20542af0e129876f5ee222176340a6594da
eoeo0326 <eoeo0326@gmail.com>
2025-02-06 18:11:24 +0900

    issue #103 feat: Profile Data Type 추가

---
+++ app/src/main/java/cord/eoeo/momentwo/domain/model/Profile.kt
@@ -0,0 +1,9 @@
+package cord.eoeo.momentwo.domain.model
+
+data class Profile(
+    val name: String,
+    val email: String,
+    val nickname: String,
+    val phone: String,
+    val profileImage: String,
+)
